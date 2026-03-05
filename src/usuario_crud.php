2c8ad503de670f19cc9ef628988a00a3aa635fa5
Breno0409-Byte <brenobr0409@gmail.com>
2026-03-05 17:47:57 -0300

    update2

--- src/usuario_crud.php
+++ src/usuario_crud.php
@@ -1,10 +1,17 @@
 <?php 
 //usuario_crud.php
 
-    function buscarUsuario()
+    function buscarUsuario(PDO $conexao)
     {
 
+    $sql = "SELECT id, nome, email FROM usuarios ORDER BY nome";
 
+    //stmt
+    //queery
+
+    $consulta = $conexao->prepare($sql);
+    $consulta->execute();
+    return $consulta->fetchAll(PDO::FETCH_ASSOC);
     
     }
 
